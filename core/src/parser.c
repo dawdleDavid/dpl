24d79f00dbca05a598b1868c61d0af5a97083629
David JR <david@DavidDator.daviddator.net>
2024-03-03 17:40:18 +0100

    söndag-mar-3(1)

--- core/src/parser.c
+++ core/src/parser.c
@@ -60,7 +60,6 @@ struct Tape* findKeyword(struct Tape* memwrap, char* buffer){
     int i = 0; // ha, didn't know that works...
     for(;i <= ROW_LIMIT; i++){
         strcpy(memwrap->words_list[i], words_list[i]);
-        printf("%s\n", memwrap->words_list[i]);
         if(strstr(memwrap->words_list[i], "\n") != NULL){
             break;
         }
@@ -78,35 +77,49 @@ void mtplParse(FILE* file){
     puts("we ran this..");
     char buffer[ROW_LIMIT];
 
+
+    uint32_t p_counter = 0;
+
+
+
+
+    /* Acctual parsing starts here... */
     while(true){
-    char* row = fgets(buffer, ROW_LIMIT, file);
+        char* row = fgets(buffer, ROW_LIMIT, file);
 
-    if(row == (char*)NULL){
-        printf("EOF\n");
-        break;
-    }
-        // do something with the line that you just got
-    memwrap_p = findKeyword(&memwrap, row);
-    char varc[ROW_LIMIT];
-    // extract value from string
+        p_counter++;
+        printf("PC: %d\n", p_counter);
+        if(row == (char*)NULL){
+            printf("EOF\n");
+            break;
+        }
+            // do something with the line that you just got
+        memwrap_p = findKeyword(&memwrap, row);
+        char varc[ROW_LIMIT];
+        // extract value from string
 
-    for(int index = 0;index <= ROW_LIMIT; index++){
-        printf("%s\n", memwrap_p->words_list[index]);
+        for( int index = 0;index <= ROW_LIMIT; index++){
+            printf("%s\n", memwrap_p->words_list[index]);
 
 
-        for(int vari = 0; vari <= sizeof(vardefs); vari++){
-            if(strcmp(memwrap_p->words_list[index], vardefs[vari]) == 0){ // oh, i guess that space is to my
-                if(index < ROW_LIMIT){
-                    strcpy(varc, memwrap_p->words_list[index+1]);
-                    break;
+
+            assert(memwrap_p->words_list[index] != NULL);
+
+            if(strstr(memwrap_p->words_list[index], "\n") != NULL){
+                break;
+            }
+            for(int vari = 0; vari <= 9; vari++){ /* assert for the win i guess!*/
+                assert(vardefs[vari] != NULL);
+                if(strcmp(memwrap_p->words_list[index], vardefs[vari]) == 0){ // oh, i guess that space is to my
+                    if(index < ROW_LIMIT){
+                        strcpy(varc, memwrap_p->words_list[index+1]);
+                        break;
+                    }
+
                 }
-
             }
         }
-    }
-    if(strstr(memwrap_p->words_list[index], "\n") != NULL){
-        break;
-    }
-    printf("->%s\n", varc);
+
+        printf("->%s\n", varc);
     }//endwhile
 }
